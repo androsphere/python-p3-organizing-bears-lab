766b32c5ee028dfe28235a7bbbb245785df81af4
Andrew MacIsaac <amacisaac95@gmail.com>
2024-01-30 16:12:28 -0700

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Winnie', 30, 'M', 'yellow', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Yogi', 5, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Smokey', 5, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Paddington', 5, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Baloo', 5, 'M', 'black', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Fozzie', 5, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Po', 5, 'M', 'black/white', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'M', 'brown', 'friendly', true);
